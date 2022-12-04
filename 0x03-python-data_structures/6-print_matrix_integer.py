5d6be597607597f71a72b34d8d4f98cd931ec57a
dekzyd <adekoyadayo2010@gmail.com>
2022-12-04 09:36:38 -0800

    find last ele

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for ele in matrix:
         for ment in ele:
+            last_in_list = len(ele)
+            if ele.index(ment) == last_in_list - 1:
+                print("{:d}".format(ment))
+                continue
             print("{:d}".format(ment), end=" ")
-        print()
