5da408cb7f1f1462a01c0a64c649432a98974781
dekzyd <adekoyadayo2010@gmail.com>
2022-12-04 15:31:31 -0800

    Update 8-multi

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = 0
-    f_char = None
+    first = None
     if len(sentence) > 1
         length = len(sentence)
         first = sentence[0]
