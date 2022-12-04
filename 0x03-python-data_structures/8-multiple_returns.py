f88b8880862d490bff142ba2823d6a8ac38700e1
dekzyd <adekoyadayo2010@gmail.com>
2022-12-04 15:39:12 -0800

    fix if bug 8-multi

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = 0
-    first = None
-    if len(sentence) > 1:
-        length = len(sentence)
-        first = sentence[0]
-    return (length, first)
+    if sentence:
+        length = 0
+        first = None
+        if len(sentence) > 1:
+            length = len(sentence)
+            first = sentence[0]
+        return (length, first)
