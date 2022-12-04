49f7c257db37d63aaf4b430f41641ed1f891a96a
dekzyd <adekoyadayo2010@gmail.com>
2022-12-04 09:38:54 -0800

    if matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for ele in matrix:
-        for ment in ele:
-            last_in_list = len(ele)
-            if ele.index(ment) == last_in_list - 1:
-                print("{:d}".format(ment))
-                continue
-            print("{:d}".format(ment), end=" ")
+    if matrix:
+        for ele in matrix:
+            for ment in ele:
+                last_in_list = len(ele)
+                if ele.index(ment) == last_in_list - 1:
+                    print("{:d}".format(ment))
+                    continue
+                print("{:d}".format(ment), end=" ")
