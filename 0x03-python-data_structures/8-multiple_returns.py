000ffdcf70f36bed09905f70d0763862c62e5705
dekzyd <adekoyadayo2010@gmail.com>
2022-12-04 15:41:51 -0800

    hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        length = 0
-        first = None
-        if len(sentence) > 1:
-            length = len(sentence)
-            first = sentence[0]
-        return (length, first)
+    length = 0
+    first = None
+    if len(sentence) >= 1:
+        length = len(sentence)
+        first = sentence[0]
+    return (length, first)
