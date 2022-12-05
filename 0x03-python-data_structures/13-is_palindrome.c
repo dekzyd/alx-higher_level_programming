7004f8e03948cccc10624fe9d6fda0929ac95f5c
dekzyd <adekoyadayo2010@gmail.com>
2022-12-05 00:51:48 -0800

    create 13-is_pal

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * is_palindrome - checks if linked list is palindrome
+ * @head: the head of the list
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	int length;
+	int count = 0;
+	int i;
+	listint_t *ptr;
+
+	ptr = *head;
+
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+
+	length = count + 1;
+	int arr[length];
+
+	ptr = *head;
+	count = 0;
+	while (ptr != NULL)
+	{
+		arr[count] = ptr->n;
+		count++;
+		ptr = ptr->next;
+	}
+	for (i = 0; i < count / 2; i++)
+	{
+		if (arr[i] == arr[(count - 1) - i])
+		{
+			continue;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
