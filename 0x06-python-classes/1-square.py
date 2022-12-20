d88bc710fa4ec4384711b7b4ca8a6ea2c5a52639
dekzyd <adekoyadayo2010@gmail.com>
2022-12-20 13:38:57 -0800

    create 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Square:
+    """creates a square with private attr"""
+    def __init__(self, size):
+        """defines the size attr of Square class"""
+        self.__size = size
