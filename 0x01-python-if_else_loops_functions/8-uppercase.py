4ceae3c3077fa593e1bb6716bed0ea8f0f3e522b
Oladayo <adekoyadayo2010@gmail.com>
2023-08-10 04:23:27 -0700

    create_task_8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = []
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            new_str.append(chr(ord(i) - 32))
+        else:
+            new_str.append(chr(ord(i)))
+    result = ''.join(new_str)
+    print(result)
