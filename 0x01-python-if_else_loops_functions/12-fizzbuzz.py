207fe420b95d014d8fe13210f965e8aa4d794d3d
Oladayo <adekoyadayo2010@gmail.com>
2023-08-10 05:26:10 -0700

    fix_task_12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 5 == 0 and i % 3 == 0:
-            print("{}".format('Fizz Buzz'), end=" ")
+            print("{}".format('FizzBuzz'), end=" ")
         elif i % 5 == 0:
             print("{}".format('Buzz'), end=" ")
         elif i % 3 == 0:
