e270242d4bcbbe8d5cd6b9b31ccbb78ea50f8d78
Oladayo <adekoyadayo2010@gmail.com>
2023-08-11 00:34:30 -0700

    create_task_3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print(0)
+    elif len(argv) == 2:
+        print(int(argv[1]))
+    else:
+        total = 0
+        for i in range(1, len(argv)):
+            total += int(argv[i])
+        print(total)
