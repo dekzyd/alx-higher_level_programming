1284c4677dd11ab52c1323d36328bb1e46415cf2
Oladayo <adekoyadayo2010@gmail.com>
2023-08-13 08:38:54 -0700

    refix_task_3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) < 1:
-        print("{}".format(my_list))
-    elif len(my_list) == 1:
+    if len(my_list) == 1:
         print("{:d}".format(my_list[0]))
     else:
         my_list.reverse()
