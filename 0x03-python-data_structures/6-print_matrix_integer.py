e06f034e44f515f7d242083957779214046e73e7
Oladayo <adekoyadayo2010@gmail.com>
2023-08-14 03:30:26 -0700

    refix_task_6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in (matrix):
         for i, j in enumerate(row):
             if i < len(row) - 1:
-                print("{:d}".format(j), end="")
+                print("{:d}".format(j), end=" ")
             else:
                 print("{:d}".format(j))
