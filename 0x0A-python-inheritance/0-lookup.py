0c68058e1eb03bcea0aeaeec4adebba3d95f2afd
Oladayo <adekoyadayo2010@gmail.com>
2023-09-11 12:49:35 -0700

    reupdate_0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' function that prints attributes and methods of an object '''
 
+
 def lookup(obj):
     '''function: lookup() returns list of the object args'''
     return dir(obj)
