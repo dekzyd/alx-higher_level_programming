7c1f9bcb1994b2f02340f40c13dd8135141cab1d
Oladayo <adekoyadayo2010@gmail.com>
2023-09-11 17:50:28 -0700

    create_2-is-same-class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''verifies if obj is instance of a class'''
+
+
+def is_same_class(obj, a_class):
+    '''function: is_same_class() returns true or false'''
+    return isinstance(obj, a_class)
