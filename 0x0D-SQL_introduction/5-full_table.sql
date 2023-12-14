0fa147d33bac5373367ac18b89b6286cad6ef4a1
Oladayo <adekoyadayo2010@gmail.com>
2023-12-14 14:53:17 -0800

    update_task_5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- prints the full description of the table 
 -- not allowed to use the DESCRIBE or EXPLAIN statements
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
+SHOW CREATE TABLE first_table
