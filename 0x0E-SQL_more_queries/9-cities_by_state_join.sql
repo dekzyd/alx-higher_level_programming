3a27da0e56f32efbcb31463eb14fd837537a7868
Oladayo <adekoyadayo2010@gmail.com>
2023-12-15 08:20:12 -0800

    init_commit_task_9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+
+SELECT c.id, c.name, s.name
+FROM hbtn_0d_usa.cities c JOIN hbtn_0d_usa.states s ON c.state_id = s.id; 
